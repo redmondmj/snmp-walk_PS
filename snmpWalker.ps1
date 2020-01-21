6be5d5ca9b80a50702673b62424ac23f876438d0
Matt Redmond <Matthew.redmond@nscc.ca>
2020-01-21 07:08:54 -0400

    Ini

---
+++ snmpWalker.ps1
@@ -0,0 +1,63 @@
+
+   do {
+    cls
+    write-host ""
+    write-host "****************************"
+    write-host "**       Set Target       **"
+    write-host "****************************"
+    write-host ""
+
+
+    write-host ""
+    $IP = read-host "Enter IP"
+    
+    $Octet = '(?:0?0?[0-9]|0?[1-9][0-9]|1[0-9]{2}|2[0-5][0-5]|2[0-4][0-9])'
+    [regex] $IPv4Regex = "^(?:$Octet\.){3}$Octet$"
+
+    $ok = $IP -match $IPv4Regex
+    if ( -not $ok) {write-host  -ForegroundColor Red "Invalid IP"
+                    sleep 2
+                    write-host ""
+                    }
+    } until ($ok)
+
+   do {
+    cls
+    write-host ""
+    write-host "****************************"
+    write-host "**       SNMP Walker        **"
+    write-host "****************************"
+    write-host ""
+
+    write-host "1 - Printer Manufacturer"
+    write-host "2 - Printer Model"
+    write-host "3 - Printer Serial"
+    write-host "4 - Printer Manager"
+    write-host "5 - Printer Location"
+    write-host "6 - Toner Level"
+    write-host "7 - Page Count"
+    write-host ""
+    write-host "8 - Errors"
+    write-host ""
+    write-host "9 - Exit"
+
+    write-host ""
+    $answer = read-host "Select number(s)"
+
+    $ok = $answer -match '[123456789]+$'
+    if ( -not $ok) {write-host  -ForegroundColor Red "Invalid selection"
+                    sleep 2
+                    write-host ""
+                    }
+    } until ($ok)
+    
+        switch -Regex ( $answer ) {
+    "1" {invoke-snmpwalk -IP 172.16.144.125 -OID 1.3.6.1.2.1.1.1 | Out-File -FilePath ".\PrinterData-$IP.txt" -Append}
+    "2" {invoke-snmpwalk -IP 172.16.144.125 -OID 1.3.6.1.4.1.1347.43.5.1.1.1 | Out-File -FilePath ".\PrinterData-$IP.txt" -Append}
+    "3" {invoke-snmpwalk -IP 172.16.144.125 -OID 1.3.6.1.4.1.1347.43.5.1.1.28.1 | Out-File -FilePath ".\PrinterData-$IP.txt" -Append}
+    "4" {tbd}
+    "5" {invoke-snmpwalk -IP 172.16.144.125 -OID 1.3.6.1.4.1.1347.43.20.7.1.5 | Out-File -FilePath ".\PrinterData-$IP.txt" -Append}
+    "6" {tbd}
+    "7" {tbd}
+    "8" {tbd}
+    }
