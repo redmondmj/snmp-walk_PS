2922d1202b92322dc37aa6d9cf046ec4047c7e18
Matt Redmond <matt.redmond@nscc.ca>
2020-01-24 07:18:05 -0400

    Added comments

--- autoWalker/hint.ps1
+++ autoWalker/hint.ps1
@@ -1,9 +1,9 @@
 # Did you know you could define parameters that can passed to you script when you execute it?
-param([string]$ip="172.16.144.125") #Must be the first statement in your script
-param([datetime]$time)
-
+param([string]$ip="172.16.144.125") #This must be the first statement in your script
 # now you can run the script and specify an IP like this:
 # powershell.exe -file autoWalker.ps1 -IP 10.0.0.1
+
+
 Write-Host "Executing script for target:"
 Write-Host $ip
 start-sleep 2
--- deviceExamples/readme.md
+++ deviceExamples/readme.md
@@ -1,6 +1,6 @@
 # Device Examples
 
-This folder contains examples of custom script intended to collect data from specific devices.
+This folder contains examples of custom scripts intended to collect data from specific devices.
 
 Each example will have it's own directory, script (snmpDevice.ps1) and README.md.
 
