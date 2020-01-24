af0544478549769cb5adbd0877f700fa97d796b4
lawyer <lawyer@WeRLegal.ca>
2020-01-24 15:32:14 -0400

    new file:   PrinterData-172.16.144.125.txt 	renamed:    snmpWalker.ps1 -> snmpWalker-BrodyRethy.ps1 	modified:   tonerDemo.ps1

--- snmpWalker-BrodyRethy.ps1
+++ snmpWalker-BrodyRethy.ps1
@@ -3,11 +3,11 @@ $oidManufacturer = "1.3.6.1.2.1.1.1"
 $oidModel = "1.3.6.1.4.1.1347.43.5.1.1.36"
 $oidSerial = "1.3.6.1.4.1.1347.43.5.1.1.28"
 $oidName = "1.3.6.1.4.1.1347.40.10.1.1.5"
-
-
+$oidSomething = ""
+$oidToner = "1.3.6.1.2.1.43.11.1.1.9.1.1"
 
 do { #Input validation for IP
-    cls
+    Clear-Host
     #draw header
     write-host ""
     write-host "********************************"
@@ -49,7 +49,7 @@ do {
         write-host "2 - Printer Model"
         write-host "3 - Printer Serial"
         write-host "4 - Printer Name"
-        write-host "5 - Printer "
+        write-host "5 - Printer Location"
         write-host "6 - Toner Level"
         write-host "7 - Page Count"
         write-host ""
@@ -72,18 +72,14 @@ do {
         
     #Compare $answer to the following cases and execute the associated command.
     switch -Regex ( $answer ) {
-    "1" {invoke-snmpwalk -IP 172.16.144.125 -OID $oidManufacturer | Out-File -FilePath ".\PrinterData-$IP.txt" -Append}
+    "1" {invoke-snmpwalk -IP $IP -OID $oidManufacturer | Out-File -FilePath ".\PrinterData-$IP.txt" -Append}
     "2" {invoke-snmpwalk -IP $IP -OID $oidModel | Out-File -FilePath ".\PrinterData-$IP.txt" -Append}
     "3" {invoke-snmpwalk -IP $IP -OID $oidSerial | Out-File -FilePath ".\PrinterData-$IP.txt" -Append}
-    "4" {invoke-snmpwalk -IP $IP -OID $oidSerial | Out-File -FilePath ".\PrinterData-$IP.txt" -Append}
-    "5" {invoke-snmpwalk -IP $IP -OID $oidName | Out-File -FilePath ".\PrinterData-$IP.txt" -Append}
-    "6" {invoke-snmpwalk -IP $IP -OID $oidName | Out-File -FilePath ".\PrinterData-$IP.txt" -Append}
+    "4" {invoke-snmpwalk -IP $IP -OID $oidName | Out-File -FilePath ".\PrinterData-$IP.txt" -Append}
+    "5" {invoke-snmpwalk -IP $IP -OID $oidLocation | Out-File -FilePath ".\PrinterData-$IP.txt" -Append}
+    "6" {invoke-snmpwalk -IP $IP -OID $oidToner | Out-File -FilePath ".\PrinterData-$IP.txt" -Append}
     "7" {write-output "Huh? What's a page count? Coming Soon" | Out-File -FilePath ".\PrinterData-$IP.txt" -Append}
     "8" {write-output "HUH? Does not compute... Coming Soon" | Out-File -FilePath ".\PrinterData-$IP.txt" -Append}
     } #finished executing menu options
 
 } until ($answer -eq 9) #User selected option 9 to exit.
-       
-    
-       
-    
