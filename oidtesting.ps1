b66aa52c8b6b7bd97bf3a97a2cdb23a69e9d673e
Ryan <38814017+tech005@users.noreply.github.com>
2020-01-24 19:18:10 -0400

    added a file for testing

---
+++ oidtesting.ps1
@@ -0,0 +1,15 @@
+param([string]$ip="172.16.144.125")
+$oidToner = "1.3.6.1.2.1.43.11.1.1.9.1.1"
+
+function tonerLevel {
+    param (
+        $oidToner
+    )
+    $toner = Get-SnmpData -IP $ip -OID $oidToner
+    $tonerData = $toner."Data"
+    $tonerLevel = 100 * $tonerData/20000
+    return $tonerLevel
+}
+
+
+invoke-snmpwalk -IP $IP -Oid
