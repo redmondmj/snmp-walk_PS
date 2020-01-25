c6e6187cc82b001025a90b75063415a4e3ea3a37
tech005 <38814017+tech005@users.noreply.github.com>
2020-01-25 11:21:18 -0400

    save

--- oidtesting.ps1
+++ oidtesting.ps1
@@ -1,5 +1,7 @@
-param([string]$ip="172.16.144.125")
+param([string]$ip="172.16.144.252")
 $oid = "1.3.6.1.2.1.43.11.1.1.9.1"
 
 
-Invoke-SnmpWalk -IpAddress $ip -Oid $oid | Format-Table -AutoSize
+#Invoke-SnmpWalk -IpAddress $ip -Oid $oid | Format-Table -AutoSize
+Invoke-SnmpWalk -IpAddress $ip -Oid 1.3.6
+# | Format-Table -AutoSize
