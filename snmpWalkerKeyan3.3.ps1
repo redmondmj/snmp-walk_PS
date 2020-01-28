cdc1e1d2e85fbac1497954d846d5271c99fcab80
Keyan <56980926+Keyan23@users.noreply.github.com>
2020-01-28 13:46:06 -0400

    5
    added some changes

--- snmpWalkerKeyan3.3.ps1
+++ snmpWalkerKeyan3.3.ps1
@@ -1,11 +1,40 @@
+param([string]$ip="172.16.144.125")
+
+
+
+$oidToner = "1.3.6.1.2.1.43.11.1.1.9.1.1"
+
+
+
+function tonerLevel {
+
+    param (
+
+        $oidToner
+
+    )
+
+    $toner = Get-SnmpData -IP $ip -OID $oidToner
+
+    $tonerData = $toner."Data"
+
+    $tonerLevel = 100 * $tonerData/20000
+
+    return $tonerLevel
+
+}
+
+
+
+$output = tonerLevel($oidToner)
 #Setting up known OIDs for Kyrocera Printers
 $oidManufacturer = "1.3.6.1.2.1.1.1"
 $oidModel = "1.3.6.1.4.1.1347.43.5.1.1.36"
 $oidSerial = "1.3.6.1.4.1.1347.43.5.1.1.28"
 $oidName = "1.3.6.1.4.1.1347.40.10.1.1.5"
 $oidPrinter = "1.3.6.1.2.1.43.15.1.1.5.1.1"
-$oidInk = "1.3.6.1.2.1.43.11.1.1.9.1.1 "
-$oidPages = ""
+$oidToner = "1.3.6.1.2.1.43.11.1.1.9.1.1 "
+$oidPages = "1.3.6.1.4.1.1347.43.10.1.1.12.1"
 $oidLocation = "1.3.6.1.2.1.1.6"
 
 
@@ -72,6 +101,7 @@ do {
             write-host ""
         }
     } until ($ok) #verified $answer
+
         
     #Compare $answer to the following cases and execute the associated command.
     switch -Regex ( $answer ) {
@@ -80,7 +110,7 @@ do {
     "3" {invoke-snmpwalk -IP $IP -OID $oidSerial | Out-File -FilePath ".\PrinterData-$IP.txt" -Append}
     "4" {invoke-snmpwalk -IP $IP -OID $oidName | Out-File -FilePath ".\PrinterData-$IP.txt" -Append}
     "5" {invoke-snmpwalk -IP $IP -OID $oidPrinter | Out-File -FilePath ".\PrinterData-$IP.txt" -Append}
-    "6" {invoke-snmpwalk -IP $IP -OID $oidInk | Out-File -FilePath ".\PrinterData-$IP.txt" -Append}
+    "6" {Write-Host "Toner ink is at $output%." | Out-File -FilePath ".\PrinterData-$IP.txt" -Append}
     "7" {invoke-snmpwalk -IP $IP -OID $oidLocation | Out-File -FilePath ".\PrinterData-$IP.txt" -Append}
     "8" {invoke-snmpwalk -IP $IP -OID $oidPages | Out-File -FilePath ".\PrinterData-$IP.txt" -Append}
     } #finished executing menu options
