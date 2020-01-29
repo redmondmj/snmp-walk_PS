95238b15e9cc592290bf7e89511f2db663fc8540
tech005 <38814017+tech005@users.noreply.github.com>
2020-01-29 18:35:21 -0400

    cleaning up

--- oidtesting-RyansLearningPortal.ps1
+++
@@ -1,5 +0,0 @@
-param([string]$ip="172.16.144.125")
-$oid = "1.3.6.1.2"
-
-
-Invoke-SnmpWalk -IpAddress $ip | Format-Table -AutoSize > fullwalk.txt
--- oidtesting.ps1
+++
@@ -1,7 +0,0 @@
-param([string]$ip="172.16.144.251")
-$oid = "1.3.6.1.2.1.1.4"
-
-
-# Invoke-SnmpWalk -IpAddress $ip -Oid $oid 
-Invoke-SnmpWalk -IpAddress $ip -Community EntMan | Format-Table -AutoSize
-# | Format-Table -AutoSize
