800cbd7a1db9ce4dcc937dc1eda6c3de78955881
kuraiskrap <56841907+kuraiskrap@users.noreply.github.com>
2020-01-31 15:20:08 -0400

    sum
    its due, its  what i got
    switch txt document isnt being made for some reason

--- deviceExamples/corey/snmpWalker.ps1
+++ deviceExamples/corey/snmpWalker.ps1
@@ -5,31 +5,28 @@ $oidSerial = "1.3.6.1.4.1.1347.43.5.1.1.28"
 $oidName = "1.3.6.1.4.1.1347.40.10.1.1.5"
 
 #my modifications
-$freememory = "1.3.6.1.4.1.674.10895.5000.2.6132.1.1.1.1.4.1"
-$totalmemory = "1.3.6.1.4.1.674.10895.5000.2.6132.1.1.1.1.4.2"
-$CPUuse = "1.3.6.1.4.1.674.10895.5000.2.6132.1.1.1.1.4.3"
+$pagecount = "1.3.6.1.2.1.43.10.2.1.4"
+$tonercapacity = "1.3.6.1.2.1.43.11.1.1.8"
+$tonerlevel = "1.3.6.1.2.1.43.11.1.1.8"
 #$tonerpercent = $tonercapacity / $tonerlevel
-$temp = "1.3.6.1.4.1.674.10895.5000.2.6132.1.1.43.1.8.1.2"
-
-#switch
-#Invoke-SnmpWalk -IP 172.16.144.251 -Community EntMan
+$location = "1.3.6.1.2.1.1.6"
 
 do { #Input validation for IP
-    Clear-Host
+    cls
     #draw header
     write-host ""
     write-host "********************************"
-    write-host "**      SNMP DEll switch      **"
+    write-host "**         Set Target         **"
     write-host "**                            **"
     write-host "**   SNSMP Package Required   **"
     write-host "**       Try Proxx SNMP       **"
     write-host "********************************"
     
     write-host ""
-    get input from user
+    #get input from user
     $IP = read-host "Enter IP"
-    #$IP = "172.16.144.251"
-    regex for checking ip
+    
+    #regex for checking ip
     $Octet = '(?:0?0?[0-9]|0?[1-9][0-9]|1[0-9]{2}|2[0-5][0-5]|2[0-4][0-9])'
     [regex] $IPv4Regex = "^(?:$Octet\.){3}$Octet$"
     
@@ -37,7 +34,7 @@ do { #Input validation for IP
     $ok = $IP -match $IPv4Regex
     if ( -not $ok) {
         write-host  -ForegroundColor Red "Invalid IP"
-        start-sleep 2
+        sleep 2
         write-host ""
     }
 } until ($ok) #verified IP input
@@ -53,13 +50,13 @@ do {
         write-host "******************************"
         write-host ""
 
-        write-host "1 - Switch Manufacturer"
-        write-host "2 - Switch Model"
-        write-host "3 - Switch Serial"
-        write-host "4 - free memory"
-        write-host "5 - total memory"
-        write-host "6 - CPU use"
-        write-host "7 - temp"
+        write-host "1 - Printer Manufacturer"
+        write-host "2 - Printer Model"
+        write-host "3 - Printer Serial"
+        write-host "4 - Printer Name"
+        write-host "5 - Printer Location"
+        write-host "6 - Toner Level"
+        write-host "7 - Page Count"
         write-host ""
         write-host "8 - Errors"
         write-host ""
@@ -73,21 +70,28 @@ do {
         #if the provided answer is not numeric 1-9 notify user and return to menu
         if ( -not $ok) {
             write-host  -ForegroundColor Red "Invalid selection"
-            Start-Sleep 2
+            sleep 2
             write-host ""
         }
     } until ($ok) #verified $answer
         
     #Compare $answer to the following cases and execute the associated command.
     switch -Regex ( $answer ) {
-    "1" {invoke-snmpwalk -IP $IP -OID $oidManufacturer | Out-File -FilePath ".\SwitchData-$IP.txt" -Append}
-    "2" {invoke-snmpwalk -IP $IP -OID $oidModel | Out-File -FilePath ".\SwitchData-$IP.txt" -Append}
-    "3" {invoke-snmpwalk -IP $IP -OID $oidSerial | Out-File -FilePath ".\SwitchData-$IP.txt" -Append}
-    "4" {invoke-snmpwalk -IP $IP -OID $freememory | Out-File -FilePath ".\SwitchData-$IP.txt" -Append}
-    "5" {invoke-snmpwalk -IP $IP -OID $totalmemory | Out-File -FilePath ".\SwitchData-$IP.txt" -Append}
-    "6" {invoke-snmpwalk -IP $IP -OID $CPUuse | Out-File -FilePath ".\SwitchData-$IP.txt" -Append        }
-    "7" {invoke-snmpwalk -IP $IP -OID $temp  | Out-File -FilePath ".\SwitchData-$IP.txt" -Append}
-    "8" {write-output "HUH? Does not compute... Coming Soon" | Out-File -FilePath ".\SwitchData-$IP.txt" -Append}
+    "1" {invoke-snmpwalk -IP 172.16.144.125 -OID $oidManufacturer | Out-File -FilePath ".\PrinterData-$IP.txt" -Append}
+    "2" {invoke-snmpwalk -IP $IP -OID $oidModel | Out-File -FilePath ".\PrinterData-$IP.txt" -Append}
+    "3" {invoke-snmpwalk -IP $IP -OID $oidSerial | Out-File -FilePath ".\PrinterData-$IP.txt" -Append}
+    "4" {invoke-snmpwalk -IP $IP -OID $oidSerial | Out-File -FilePath ".\PrinterData-$IP.txt" -Append}
+    "5" {invoke-snmpwalk -IP $IP -OID $location | Out-File -FilePath ".\PrinterData-$IP.txt" -Append}
+    "6" {
+        invoke-snmpwalk -IP $IP -OID $tonerlevel
+        invoke-snmpwalk -IP $IP -OID $tonercapacity
+        $tonercapacity / $tonerlevel | Out-File -FilePath ".\PrinterData-$IP.txt" -Append}
+    "7" {write-output IP $IP -OID $pagecount  | Out-File -FilePath ".\PrinterData-$IP.txt" -Append}
+    "8" {write-output "HUH? Does not compute... Coming Soon" | Out-File -FilePath ".\PrinterData-$IP.txt" -Append}
     } #finished executing menu options
 
 } until ($answer -eq 9) #User selected option 9 to exit.
+       
+    
+       
+    
