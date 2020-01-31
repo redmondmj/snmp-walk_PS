2ab1ede8373ba3fe1aec77899c934a485ab063a1
iisfire1 <56842185+iisfire1@users.noreply.github.com>
2020-01-31 15:09:23 -0400

    add automation
    added ps1 scrpit for getting info for the switch and automating that process

---
+++ autoWalker/SNMP auto/auto SNMP info.ps1
@@ -0,0 +1,10 @@
+﻿#used to use the inputed Ip address
+param([string]$ip="172.16.144.251")
+
+#the path and program to lanuch in order to use the script
+$task = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument '-File "C:\Users\erich\Downloads\snmp-walk_PS\deviceExamples\SNMPswitchex\SWITCHinfo.ps1"'
+
+#take is set to trigger at 12AM daily
+$trigger = New-ScheduledTasktrigger -Daily -At 12AM
+#making the tas show up in task scheduler and assignging the task with an action.
+Register-ScheduledTask -Action $task -Trigger $trigger -TaskName "SNMPTest" -Description "Automateing SNMP Script"  
