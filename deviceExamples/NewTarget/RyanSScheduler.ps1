b289f5e0af3fbe28f872149d7809a803c610fb91
ryans9512 <56842047+ryans9512@users.noreply.github.com>
2020-01-31 14:11:34 -0400

    Update RyanSScheduler.ps1
    corrected syntax errors

--- deviceExamples/NewTarget/RyanSScheduler.ps1
+++ deviceExamples/NewTarget/RyanSScheduler.ps1
@@ -1,7 +1,7 @@
 param([string]$ip="172.16.144.251")
-$action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument 'C:\Users\ryan_\Documents\GitHub\snmp-walk_PS\deviceExamples\NewTarget\RyanSCustomSNMP.ps1'
+$action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument '-File "C:\Users\ryan_\Documents\GitHub\snmp-walk_PS\deviceExamples\NewTarget\RyanSCustomSNMP.ps1"'
 
 
-$trigger = New-ScheduledTaskTrigger -Daily -At 2:04PM
-Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "SNMPAuto2" -Description "Automated SNMP Script"    
-    
+$trigger = New-ScheduledTaskTrigger -Daily -At 2:10PM
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "SNMPAutoTest" -Description "Automated SNMP Script"    
+   
