fff707bed10c113acaaefd4390ff109f7659bd11
ryans9512 <56842047+ryans9512@users.noreply.github.com>
2020-01-31 14:15:51 -0400

    Update RyanSScheduler.ps1
    added comments to describe script actions

--- deviceExamples/NewTarget/RyanSScheduler.ps1
+++ deviceExamples/NewTarget/RyanSScheduler.ps1
@@ -1,7 +1,10 @@
+#ip of switch given as parameter
 param([string]$ip="172.16.144.251")
+
+#creating action to execute powershell, and then run the selected custom snmp powershell script
 $action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument '-File "C:\Users\ryan_\Documents\GitHub\snmp-walk_PS\deviceExamples\NewTarget\RyanSCustomSNMP.ps1"'
-
-
-$trigger = New-ScheduledTaskTrigger -Daily -At 2:10PM
+#set task to trigger daily at 3pm
+$trigger = New-ScheduledTaskTrigger -Daily -At 3PM
+#registering the task as well as asssigning the trigger and action to the task. Adding a name and description to task
 Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "SNMPAutoTest" -Description "Automated SNMP Script"    
    
